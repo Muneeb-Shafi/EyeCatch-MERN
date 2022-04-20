e4f872762c29fe2661baea870af2c86054f5444d
Muneeb-Shafi <f190959@nu.edu.pk>
2022-04-21 02:07:38 +0500

    UI Changes

---
+++ src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "Kawai Anime Girl",
+    image: "/images/1.png",
+    description:
+      "Mai San, is from an anime called Buta Yaro wa Bunny Girl Senpai. She likes to dress as exotic Bunny girls and roam arround the city",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Kawai Anime Girl",
+    image: "/images/1.png",
+    description:
+      "Mai San, is from an anime called Buta Yaro wa Bunny Girl Senpai. She likes to dress as exotic Bunny girls and roam arround the city",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Kawai Anime Girl",
+    image: "/images/1.png",
+    description:
+      "Mai San, is from an anime called Buta Yaro wa Bunny Girl Senpai. She likes to dress as exotic Bunny girls and roam arround the city",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Kawai Anime Girl",
+    image: "/images/1.png",
+    description:
+      "Mai San, is from an anime called Buta Yaro wa Bunny Girl Senpai. She likes to dress as exotic Bunny girls and roam arround the city",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "Kawai Anime Girl",
+    image: "/images/1.png",
+    description:
+      "Mai San, is from an anime called Buta Yaro wa Bunny Girl Senpai. She likes to dress as exotic Bunny girls and roam arround the city",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Kawai Anime Girl",
+    image: "/images/1.png",
+    description:
+      "Mai San, is from an anime called Buta Yaro wa Bunny Girl Senpai. She likes to dress as exotic Bunny girls and roam arround the city",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
